13423a14f22071c1ece5659a00c1f93b3115fb58
n-s-harvey <150607031+n-s-harvey@users.noreply.github.com>
2024-02-06 20:10:48 -0600

    Drum-pad triggers on uppercase or lowercase key inputs

--- src/features/drum-pad.js
+++ src/features/drum-pad.js
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Sound from "./sound";
 import useSound from "use-sound";
+import { useEffect } from "react";
 
 /**
  * @param {{sound: Sound}} props
@@ -9,6 +10,17 @@ import useSound from "use-sound";
 export default function DrumPad(props) {
   const soundURL = props.sound.src;
   const [playSound] = useSound(soundURL);
+
+  useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.key.toUpperCase() == props.sound.key) playSound();
+    }
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }
+  )
   return (
     <Button className="drum-pad" id={props.sound.id} onClick={playSound}>
       {props.sound.key}
