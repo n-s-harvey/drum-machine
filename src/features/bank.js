b42b1e8ad31ee75eb3972048fd0dad79887ac73b
n-s-harvey <150607031+n-s-harvey@users.noreply.github.com>
2024-02-11 14:32:51 -0600

    Sounds renamed for better user experience

--- src/features/bank.js
+++ src/features/bank.js
@@ -11,55 +11,55 @@ const sounds = [
   new Sound(
     "heater-1",
     "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-    "heater-1",
+    "Heater #1",
     "Q"
   ),
   new Sound(
     "heater-2",
     "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-    "heater-2",
+    "Heater #2",
     "W"
   ),
   new Sound(
     "heater-3",
     "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
-    "heater-3",
+    "Heater #3",
     "E"
   ),
   new Sound(
     "heater-4",
     "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
-    "heater-4",
+    "Heater #4",
     "A"
   ),
   new Sound(
     "clap",
     "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
-    "clap",
+    "Clap",
     "S"
   ),
   new Sound(
     "open-hh",
     "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
-    "open-hh",
+    "Open Hi-hat",
     "D"
   ),
   new Sound(
     "kick-n-hat",
     "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
-    "kick-n-hat",
+    "Kick & hi-hat",
     "Z"
   ),
   new Sound(
     "kick",
     "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
-    "kick",
+    "Kick",
     "X"
   ),
   new Sound(
     "closed-hh",
     "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
-    "closed-hh",
+    "Closed hi-hat",
     "C"
   )
 
@@ -71,7 +71,7 @@ export default function Bank() {
     <Container id='bank'>
       <Row className="g-3">
         {sounds.map(
-          sound => (<Col xs={4}><DrumPad sound={sound} /></Col>)
+          sound => (<Col xs={4}><DrumPad sound={sound} key={sound.id} /></Col>)
         )}
       </Row>
     </Container>
