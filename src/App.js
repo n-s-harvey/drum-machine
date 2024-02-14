ee5949eeac26791460cd83fae91c0cfc3e205ff1
n-s-harvey <150607031+n-s-harvey@users.noreply.github.com>
2024-02-14 07:57:09 -0600

    Page title updates on page load to "Drum Machine"

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.scss'
 import Display from './features/display';
 import Bank from './features/bank';
 import Container from 'react-bootstrap/Container';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = "Drum Machine";
+  })
   return (
     <Container id='drum-machine' className='d-flex justify-content-around flex-column'>
       <Bank />
