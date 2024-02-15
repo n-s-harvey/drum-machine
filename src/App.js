4537b8088a96ca2380ac1f4ecc2abdc720a6da9f
n-s-harvey <150607031+n-s-harvey@users.noreply.github.com>
2024-02-15 08:18:35 -0600

    Page header added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss'
 import Display from './features/display';
 import Bank from './features/bank';
+import Header from './features/header';
 import Container from 'react-bootstrap/Container';
 import { useEffect } from 'react';
 
@@ -10,6 +11,7 @@ function App() {
   })
   return (
     <Container id='drum-machine' className='d-flex justify-content-around flex-column'>
+      <Header />
       <Bank />
       <Display />
     </Container>
